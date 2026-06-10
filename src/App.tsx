17f66afc6b2c137b5003ada4cbd1029482224688
ChuongPT <chuongpt@ikameglobal.com>
2026-06-10 15:51:44 +0700

    add protected rout to any page via check usertoken

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Landing from './pages/Landing';
 import Admin from './pages/Admin';
 import WeeklyPlan from './pages/WeeklyPlan';
+import { GlobalData } from './common/GlobalData';
+
+function ProtectedRoute({ element }: { element: React.ReactElement }) {
+  const token = GlobalData.getUserToken();
+  if (token == null || token === undefined || token === '') {
+    return <Navigate to="/" replace />;
+  }
+  return element;
+}
 
 export default function App() {
   return (
     <HashRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="/home" element={ <Home /> } />
-        <Route path="/admin" element={ <Admin /> } />
-        <Route path="/weekly-plan" element={ <WeeklyPlan /> } />
+        <Route path="/home" element={<ProtectedRoute element={<Home />} />} />
+        <Route path="/admin" element={<ProtectedRoute element={<Admin />} />} />
+        <Route path="/weekly-plan" element={<ProtectedRoute element={<WeeklyPlan />} />} />
       </Routes>
     </HashRouter>
   )
